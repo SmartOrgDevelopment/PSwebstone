00b8b99135e1f837fef72196b41c8951af9a4eda
Grant Steinfeld <grantsteinfeld@gmail.com>
2012-08-05 12:27:55 -0400

    Doug updated  with sweeter and more idiomatic PowerShell style

--- core/Randomness.ps1
+++ core/Randomness.ps1
@@ -1,16 +1,42 @@
-﻿function Get-Words {
-    
+﻿
+function Get-Words {
+    function ql {$args}
+    ql  "Alpha Romeo" Mazarati Porshe Volkswagen Fiat Mini BMW Toyota Mitshubishi Volvo Audi Ferrari
+}
 
-    $list = "Ferrari","Lamborghini","Alpha Romeo","Mazarati","Porshe","Ford","Volkswagen","Fiat","Mini","BMW","Toyota"
-    $list += "Lexus"
-    $list += "Mitshubishi"
-    $list += "RAM"
-    $list += "Lexus"
-    $list += "Volvo"
-    $list += "Audi"
-    $list
+ 
+
+function Get-RandomSiloDoug {
+
+   
+
+    param(
+        $size=7,
+        $min=1,
+        $max=9
+         )
+
+   
+
+    $silo = @()
+
+    1..$size | % {
+     
+      $silo += Get-Random  -Max $max
+
+    }
+
+   
+
+    Write-Host $silo -BackgroundColor DarkGreen
+
+   
+
+    $silo
 
 }
+
+ 
 function Get-RandomSilo
 {
     param($size=7,$min=1,$max=9)
@@ -24,94 +50,173 @@ function Get-RandomSilo
     $silo
   }
 
+ 
 
-function Get-Fluff
-{
-    param($len,$flufChar = " ")
+function Get-Fluff {
 
-    $s= @()
-    for ($i = 1; $i -lt $len; $i++)
-    { 
+   
+
+    param(
+
+        $len,
+
+        $flufChar = " "
+
+    )
+
+ 
+
+ 
+
+#    $s= @()
+
+#    for ($i = 1; $i -lt $len; $i++)
+
+#    {
+
+#        $s += $flufChar
+
+#    }
+
+#   
+
+#    return [string]::Join("",$s)
+
+ 
+
+    1..$len | ForEach {$s=@()} {
+
         $s += $flufChar
-    }
 
-    return [string]::Join("",$s)
+    } { [string]::Join("",$s) }
 
 }
 
+ 
 
-function Get-PaddedString
-{
-    param([string] $string,$pad=5)
+ 
+
+function Get-PaddedString {
+
+   
+
+    param(
+
+        [string]$string,
+
+        $pad=5
+
+    )
+
+ 
 
     $len = $string.Length
+
     $z = $pad - $len
+
     if($z -ge 0)
+
     {
+
         $padding = Get-Fluff $z
+
         return $string + $padding
+
     }
+
     else
+
     {
+
         #trim
+
         return $string.Substring(0,$pad)
+
     }
+
 }
 
-function Get-LongestWord
-{
+ 
+
+function Get-LongestWord {
+
+   
+
     param($list)
+
+ 
+
     $len = $list.Length
+
     $longest = 0
+
     foreach ($item in $list)
+
     {
+
         $tmpLen = $item.Length
+
         if( $tmpLen -gt $longest )
+
         {
+
             $longest = $tmpLen
+
         }
+
     }
+
+ 
 
     $longest
+
 }
 
-function Invoke-Main
-{
+ 
 
+function Invoke-Main {
     $dictionary = Get-Words
     $numberWords = $dictionary.Count
     $longestWord = Get-LongestWord $dictionary
     $longestWord++
 
-
     $listWords = @()
     $listRandom = Get-RandomSilo 100 1 $numberWords
+
     $listRandom | % {
-
         $listWords += $dictionary[$_]
-
     }
 
+ 
 
     Show-PrettyPrintDualColumn $listWords $longestWord
-   
-
-
 
 }
 
-function Show-PrettyPrintDualColumn
-{
+ 
+
+function Show-PrettyPrintDualColumn {
     param($listWords, $longestWord)
 
+
     for ($i = 1; $i -lt $listWords.Count; $i=$i +2)
-    { 
+    {
         $a = Get-PaddedString $listWords[$i] $longestWord
         $b = Get-PaddedString $listWords[$i-1] $longestWord
 
-        "$a $b"
-    }
+        #"$a $b"
 
+        New-Object PSObject -Property @{
+            a = $a
+            b = $b
+        }
+    }
 }
 
+ 
+
+cls
+
 Invoke-Main
+
+ 
